6c61746a280db583b944a609d50068a705643935
sofien12 <35785424+sofien12@users.noreply.github.com>
2021-10-01 11:33:44 +0100

    Update CheckPassword.java

--- src/main/java/com/api/sendgrid/notify/service/servicenotify/CheckPassword.java
+++ src/main/java/com/api/sendgrid/notify/service/servicenotify/CheckPassword.java
@@ -12,7 +12,6 @@ import com.api.sendgrid.notify.service.servicemessage.MessageService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
-import studio.slashup.iosis.notify.entity.dto.*;
 
 import com.api.sendgrid.notify.utils.NotifyExecpion;
 import org.apache.logging.log4j.LogManager;
